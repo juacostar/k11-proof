845d0060fb259f868f08951945e5e672da5230ca
Juan Camilo Acosta Rojas <juacostar@unal.edu.co>
2023-06-21 16:21:06 -0500

    finOneUsertest added

--- k11-proof/src/test/java/com/proof/k11proof/K11Test.java
+++ k11-proof/src/test/java/com/proof/k11proof/K11Test.java
@@ -1,2 +1,51 @@
-package com.proof.k11proof;public class K11Test {
+package com.proof.k11proof;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.proof.k11proof.model.User;
+import com.proof.k11proof.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = K11ProofApplication.class)
+@AutoConfigureMockMvc
+public class K11Test {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserService userService;
+
+    private final Jackson2ObjectMapperBuilder mapperBuilder = new Jackson2ObjectMapperBuilder();
+    private final ObjectMapper objectMapper = mapperBuilder.build();
+
+    @Test
+    void getUser() throws Exception{
+        String uri = "/k11/api/users/8";
+        User userExpected = userService.findOneUser(8);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("client_id","clientIdPrueba");
+        headers.set("client_secret","clientSecretPrueba");
+
+        ResultActions resultActions = this.mockMvc.perform(get(uri).headers(headers)).andExpect(status().isOk());
+        MvcResult mvcResult = resultActions.andReturn();
+
+        String response = mvcResult.getResponse().getContentAsString();
+        User responseObtained = objectMapper.readValue(response, User.class);
+        assertEquals(responseObtained.getId(), userExpected.getId());
+
+    }
+
 }
