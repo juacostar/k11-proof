18d39abc17ad9aadfb310375219f3a76d0f53987
Juan Camilo Acosta Rojas <juacostar@unal.edu.co>
2023-06-21 16:39:14 -0500

    addUser test

--- k11-proof/src/test/java/com/proof/k11proof/K11Test.java
+++ k11-proof/src/test/java/com/proof/k11proof/K11Test.java
@@ -1,6 +1,7 @@
 package com.proof.k11proof;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.proof.k11proof.dto.AddUserDTO;
 import com.proof.k11proof.model.User;
 import com.proof.k11proof.service.UserService;
 import org.junit.jupiter.api.Test;
@@ -8,12 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -48,4 +51,22 @@ public class K11Test {
 
     }
 
+    @Test
+    void createUser() throws Exception{
+        AddUserDTO addUserDTO = new AddUserDTO();
+        addUserDTO.setFirst_name("juan");
+        addUserDTO.setEmail("correo_prueba@gmail.com");
+
+        String uri = "/k11/api/users";
+        User userExpected = userService.addUser(addUserDTO);
+
+
+        String requestJson = objectMapper.writeValueAsString(addUserDTO);
+        ResultActions resultActions = this.mockMvc.perform(post(uri).contentType(MediaType.APPLICATION_JSON).content(requestJson)).andExpect(status().isOk());
+        MvcResult mvcResult = resultActions.andReturn();
+
+        String response = mvcResult.getResponse().getContentAsString();
+        User responseObtained = objectMapper.readValue(response, User.class);
+        assertEquals(responseObtained.getEmail(), userExpected.getEmail());
+    }
 }
